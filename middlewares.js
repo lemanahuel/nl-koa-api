176581de8755eba68217abcdc204ec4c5941e503
lemanahuel <lema.nahuel@gmail.com>
2019-03-03 08:20:19 -0300

    FIX, middlewares

--- middlewares.js
+++ middlewares.js
@@ -1,12 +1,12 @@
 
 const _ = require('lodash');
-const domains = ['localhost:5000', 'nl-koa-api.herokuapp.com', 'nl-koa-www.herokuapp.com'];
+const domains = ['http://localhost:5000', 'https://nl-koa-api.herokuapp.com', 'https://nl-koa-www.herokuapp.com'];
 
 module.exports = {
   isValidDomain: (ctx, next) => {
     if (_.indexOf(domains, ctx.request.header.origin) > -1) {
       return next();
     }
-    return ctx.badRequest({ error: 'err-invalid-origin-domain' });
+    return ctx.badRequest({ error: 'err-invalid-origin-domain', origin: ctx.request.header.origin });
   }
 };
