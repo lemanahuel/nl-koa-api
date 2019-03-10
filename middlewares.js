1b54637a663dee9ae7efcc91a2b34362a6001f16
lemanahuel <lema.nahuel@gmail.com>
2019-03-10 13:32:21 -0300

    CHANGE, whitelist

--- middlewares.js
+++ middlewares.js
@@ -1,10 +1,10 @@
 
 const _ = require('lodash');
-const domains = ['http://localhost:5000', 'https://nl-koa-api.herokuapp.com', 'https://nl-koa-www.herokuapp.com'];
+const config = require('./config/config');
 
 module.exports = {
   isValidDomain: (ctx, next) => {
-    if (_.indexOf(domains, ctx.request.header.origin) > -1) {
+    if (_.includes(config.WHITE_LIST_DOMAINS, ctx.request.header.origin)) {
       return next();
     }
     return ctx.badRequest({ error: 'err-invalid-origin-domain', origin: ctx.request.header.origin });
