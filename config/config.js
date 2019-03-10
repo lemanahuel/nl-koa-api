d63dd16cae17c0c6ef4eb8d5272de36182d5c9e5
lemanahuel <lema.nahuel@gmail.com>
2019-03-10 13:34:10 -0300

    CHANGE, whitelist

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   CLOUDINARY_URL: process.env.CLOUDINARY_URL,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   WHITE_LIST_DOMAINS: ['http://localhost:5000', 'http://localhost:5001',
-    'https://nl-static-www.herokuapp.com/',
-    'https://nl-koa-www.herokuapp.com/', 'https://nl-koa-api.herokuapp.com',
-    'https://nl-koa-www-prod.herokuapp.com/', 'https://nl-koa-api-prod.herokuapp.com']
+    'https://nl-static-www.herokuapp.com',
+    'https://nl-koa-www.herokuapp.com', 'https://nl-koa-api.herokuapp.com',
+    'https://nl-koa-www-prod.herokuapp.com', 'https://nl-koa-api-prod.herokuapp.com']
 };
